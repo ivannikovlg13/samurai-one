5e89e0590f3b093b3734069c64325469872ced60
ivannikovlg13 <livannikov55@gmail.com>
2022-06-19 08:11:32 +0300

    added first components

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import './header.css'
+
+export const Header = () => {
+  return(
+    <nav className = 'header'>
+      <img src="https://brandmark.io/logo-rank/random/bp.png" alt="logo" />
+    </nav>
+    )
+}
+
+ 
+
