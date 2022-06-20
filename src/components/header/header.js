f0db8cf2c222362f1fc52a65e56237c8b432c3e9
ivannikovlg13 <livannikov55@gmail.com>
2022-06-20 08:54:08 +0300

    make css modules

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,9 @@
-import './header.css'
+import React from 'react'
+import s from './header.module.css'
 
 export const Header = () => {
   return(
-    <nav className = 'header'>
+    <nav className = {s.header}>
       <img src="https://brandmark.io/logo-rank/random/bp.png" alt="logo" />
     </nav>
     )
