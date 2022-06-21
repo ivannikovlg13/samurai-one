0e6ebba62cb2e76d7f76d9d9a5168d252488801a
ivannikovlg13 <livannikov55@gmail.com>
2022-06-21 11:20:14 +0300

    added first props

--- src/components/profile/posts/posts.jsx
+++ src/components/profile/posts/posts.jsx
@@ -5,12 +5,10 @@ import s from './posts.module.css';
 const MyPosts = () => {
   return (
     <div className = {s.posts}>
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    </div>)
+    <Post message='How are you?' likesCount='10'/>
+    <Post message='Iam Okey !' likesCount ='20'/>
+    </div>
+    )
 }
 
 export default MyPosts
